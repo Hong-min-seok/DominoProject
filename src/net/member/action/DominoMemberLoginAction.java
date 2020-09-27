e287d26fe8b491b46c5eb8c06f4bfb0df002dbc2
Hong-min-seok <59468116+Hong-min-seok@users.noreply.github.com>
2020-09-27 19:34:55 +0900

    ver0.6
    회원가입폼 만드는중,,,

---
+++ src/net/member/action/DominoMemberLoginAction.java
@@ -0,0 +1,16 @@
+package net.member.action;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import net.main.action.ActionForward;
+
+public class DominoMemberLoginAction implements Action {
+
+	@Override
+	public ActionForward execute(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
